5193426d8f311d5ce182ff11edad401e8a6a78f9
christophior <christophior@christoiorsiMac.attlocal.net>
2022-02-17 18:15:21 -0600

    update react version and proptypes to use lib

--- examples/webpack.config.js
+++ examples/webpack.config.js
@@ -2,9 +2,8 @@ var path = require("path");
 var HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-
   entry: {
-    "basic": "./examples/basic/basic.js"
+    basic: "./examples/basic/basic.js",
   },
 
   output: {
@@ -12,31 +11,31 @@ module.exports = {
     filename: "[name].js",
     chunkFilename: "[id].chunk.js",
     sourceMapFilename: "[name].map",
-    assetPath: "/"
+    assetPath: "/",
   },
 
   resolve: {
     alias: {
-      "react-bootstrap-datetimepicker": "../../src/DateTimeField"
+      "react-bootstrap-datetimepicker": "../../src/DateTimeField",
     },
-    extensions: ["", ".js"]
+    extensions: ["", ".js"],
   },
 
   module: {
     loaders: [
-      { test: /\.js?$/, exclude: /node_modules/, loader: "babel-loader" }
-    ]
+      { test: /\.js?$/, exclude: /node_modules/, loader: "babel-loader" },
+    ],
   },
-
 
   devServer: {
     contentBase: "examples/",
-    stats: {colors: true}
+    stats: { colors: true },
   },
 
   devtool: "eval-source-map",
-  plugins: [new HtmlWebpackPlugin({
-    template: path.join(__dirname, "/basic/index.html")
-  })]
-
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "/basic/index.html"),
+    }),
+  ],
 };
